76d95b8115407f9958bc7d2f77d67ae403b0b54b
SilverStryker <rykerellingworth@gmail.com>
2025-05-18 02:13:32 -0500

    Buttons and Scoreboard in place, no functionality

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -20,6 +20,7 @@ class RockPaperScissorsLizardSpock_GameStart{
     private int opponentScore = 0;
 
     private JFrame frame;
+    private JTextField directionField;
     private JButton yesButton;
     private JButton noButton;
     private JButton rockButton;
@@ -27,27 +28,33 @@ class RockPaperScissorsLizardSpock_GameStart{
     private JButton scissorsButton;
     private JButton lizardButton;
     private JButton spockButton;
+    private JTextField yourScoreField;
+    private JTextField opponentScoreField;
+    private JLabel yourLabel;
+    private JLabel opponentLabel;
+    
 
 
     public RockPaperScissorsLizardSpock_GameStart(){
 
         frame = new JFrame("Rock, Paper, Scissors");
-        frame.setSize(500, 500);
+        frame.setSize(700, 700);
         frame.setLayout(null);
 
-        JTextField directionField = new JTextField("Would you like to play a game of Rock, Paper, Scissors, Lizard, Spock?");
-        directionField.setBounds(5, 5, 490, 100);
+        directionField = new JTextField("Would you like to play a game of Rock, Paper, Scissors, Lizard, Spock?");
+        directionField.setBounds(5, 5, 675, 100);
         directionField.setHorizontalAlignment(JTextField.CENTER);
         directionField.setFont(new Font("Arial", Font.PLAIN, 16));
+        directionField.setEditable(false);
         frame.add(directionField);
 
         yesButton = new JButton("Yes");
-        yesButton.setBounds(50, 300, 100, 100);
+        yesButton.setBounds(75, 300, 100, 100);
         yesButton.setFont(new Font("Arial", Font.PLAIN, 28));
         frame.add(yesButton);
 
         noButton = new JButton("No");
-        noButton.setBounds(325, 300, 100, 100);
+        noButton.setBounds(510, 300, 100, 100);
         noButton.setFont(new Font("Arial", Font.PLAIN, 28));
         frame.add(noButton);
 
@@ -74,6 +81,59 @@ class RockPaperScissorsLizardSpock_GameStart{
             frame.remove(noButton);
             frame.revalidate();
             frame.repaint();
+
+            directionField.setText("Let's Play! Make your choice!");
+
+            rockButton = new JButton("Rock");
+            rockButton.setBounds(295, 150, 80, 80);
+            rockButton.setHorizontalAlignment(JButton.CENTER);
+            frame.add(rockButton);
+
+            paperButton = new JButton("Paper");
+            paperButton.setBounds(75, 300, 80, 80);
+            rockButton.setHorizontalAlignment(JButton.CENTER);
+            frame.add(paperButton);
+            
+            scissorsButton = new JButton("Scissors");
+            scissorsButton.setBounds(510, 300, 80, 80);
+            scissorsButton.setHorizontalAlignment(JButton.CENTER);
+            frame.add(scissorsButton);
+
+            lizardButton = new JButton("Lizard");
+            lizardButton.setBounds(200, 450, 80, 80);
+            lizardButton.setHorizontalAlignment(JButton.CENTER);
+            frame.add(lizardButton);
+
+            spockButton = new JButton("Spock");
+            spockButton.setBounds(385, 450, 80, 80);
+            spockButton.setHorizontalAlignment(JButton.CENTER);
+            frame.add(spockButton);
+
+            yourScoreField = new JTextField("0");
+            yourScoreField.setBounds(5, 555, 100, 100);
+            yourScoreField.setHorizontalAlignment(JTextField.CENTER);
+            yourScoreField.setFont(new Font("Arial", Font.PLAIN, 32));
+            yourScoreField.setEditable(false);
+            frame.add(yourScoreField);
+
+            opponentScoreField = new JTextField("0");
+            opponentScoreField.setBounds(580, 555, 100, 100);
+            opponentScoreField.setHorizontalAlignment(JTextField.CENTER);
+            opponentScoreField.setFont(new Font("Arial", Font.PLAIN, 32));
+            opponentScoreField.setEditable(false);
+            frame.add(opponentScoreField);
+
+            yourLabel = new JLabel("You");
+            yourLabel.setBounds(5, 525, 100, 25);
+            yourLabel.setHorizontalAlignment(JLabel.CENTER);
+            yourLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+            frame.add(yourLabel);
+
+            opponentLabel = new JLabel("Opponent");
+            opponentLabel.setBounds(580, 525, 100, 25);
+            opponentLabel.setHorizontalAlignment(JLabel.CENTER);
+            opponentLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+            frame.add(opponentLabel);
 
 
         }
